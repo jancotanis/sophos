ff5759a82c52eca2ced0441cc5420c62ca498737
Janco Tanis <git@jancology.com>
2024-02-15 11:53:30 +0100

    fix correct initialization

--- lib/sophos/configuration.rb
+++ lib/sophos/configuration.rb
@@ -1,4 +1,6 @@
 require 'wrapi'
+require File.expand_path('version', __dir__)
+require File.expand_path('pagination', __dir__)
 
 module Sophos
   # Defines constants and methods related to configuration
@@ -11,10 +13,35 @@ module Sophos
     # @private
     attr_accessor *VALID_OPTIONS_KEYS
 
+    DEFAULT_ENDPOINT = 'https://api.central.sophos.com'.freeze
+    DEFAULT_ID_ENDPOINT = 'https://id.sophos.com'.freeze
+    DEFAULT_UA = "Sophos Ruby API wrapper #{Sophos::VERSION}".freeze
+    DEFAULT_PAGINATION = Sophos::RequestPagination::PagesPagination
+    DEFAULT_PAGE_SIZE = 100
+
+    # When this module is extended, set all configuration options to their default values
+    def self.extended(base)
+      base.reset
+    end
+
+    # Create a hash of options and their values
+    def options
+      VALID_OPTIONS_KEYS.inject({}) do |option, key|
+        option.merge!(key => send(key))
+      end
+    end
+
     # Reset all configuration options to defaults
     def reset
       super
       self.partner_id = nil
+      self.tenant_id = nil
+
+      self.endpoint = DEFAULT_ENDPOINT
+      self.id_endpoint = DEFAULT_ID_ENDPOINT
+      self.user_agent = DEFAULT_UA
+      self.page_size = DEFAULT_PAGE_SIZE
+      self.pagination_class = DEFAULT_PAGINATION
     end
   end
 end
--- lib/sophos_central_api.rb
+++ lib/sophos_central_api.rb
@@ -1,37 +1,14 @@
 require "wrapi"
 require File.expand_path('sophos/api', __dir__)
 require File.expand_path('sophos/client', __dir__)
-require File.expand_path('sophos/pagination', __dir__)
-require File.expand_path('sophos/version', __dir__)
 
 module Sophos
   extend Configuration
   extend WrAPI::RespondTo
 
-  DEFAULT_ENDPOINT = 'https://api.central.sophos.com'.freeze
-  DEFAULT_ID_ENDPOINT = 'https://id.sophos.com'.freeze
-  DEFAULT_UA = "Sophos Ruby API wrapper #{Sophos::VERSION}".freeze
-  DEFAULT_PAGINATION = Sophos::RequestPagination::PagesPagination
-  DEFAULT_PAGE_SIZE = 100
-
   #
   # @return [Sophos::Client]
   def self.client(options = {})
-    Sophos::Client.new({
-      endpoint: DEFAULT_ENDPOINT,
-      id_endpoint: DEFAULT_ID_ENDPOINT,
-      user_agent: DEFAULT_UA,
-      page_size: DEFAULT_PAGE_SIZE,
-      pagination_class: DEFAULT_PAGINATION
-    }.merge(options))
-  end
-
-  def self.reset
-    super
-    self.endpoint = DEFAULT_ENDPOINT
-    self.id_endpoint = DEFAULT_ID_ENDPOINT
-    self.user_agent = DEFAULT_UA
-    self.page_size = DEFAULT_PAGE_SIZE
-    self.pagination_class = DEFAULT_PAGINATION
+    Sophos::Client.new
   end
 end
